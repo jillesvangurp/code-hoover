539a6b9250fd38c30a4cc6eb74519fcb881a3d41
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2025-09-17 14:07:04 +0200

    tweak about screen

--- src/jsMain/kotlin/AboutScreen.kt
+++ src/jsMain/kotlin/AboutScreen.kt
@@ -14,29 +14,29 @@ fun RenderContext.aboutScreen() {
         h2("text-xl font-bold") { translate(DefaultLangStrings.About) }
         p { translate(DefaultLangStrings.AboutIntro) }
         div("flex flex-col items-center gap-2") {
-            a("link link-primary") {
-                href(repoUrl)
-                attr("target", "_blank")
-                translate(DefaultLangStrings.GithubRepo)
-            }
             val repoImg = img("w-32 h-32") {}
             MainScope().launch {
                 val svg = generateQrSvg(repoUrl, 200)
                 val encoded = window.btoa(svg)
                 repoImg.domNode.setAttribute("src", "data:image/svg+xml;base64,$encoded")
             }
+            a("link link-primary") {
+                href(repoUrl)
+                attr("target", "_blank")
+                translate(DefaultLangStrings.GithubRepo)
+            }
         }
         div("flex flex-col items-center gap-2") {
-            a("link link-primary") {
-                href(videoUrl)
-                attr("target", "_blank")
-                translate(DefaultLangStrings.VeritasiumVideo)
-            }
             val videoImg = img("w-32 h-32") {}
             MainScope().launch {
                 val svg = generateQrSvg(videoUrl, 200)
                 val encoded = window.btoa(svg)
                 videoImg.domNode.setAttribute("src", "data:image/svg+xml;base64,$encoded")
+            }
+            a("link link-primary") {
+                href(videoUrl)
+                attr("target", "_blank")
+                translate(DefaultLangStrings.VeritasiumVideo)
             }
         }
         p { translate(DefaultLangStrings.OpenSourceStatement) }
